b75049cf348e5727c4440363ee9d3b514118ca91
butolinka <butolinkam@gmail.com>
2023-06-17 15:44:04 -0400

    Added message form and list of messages

--- instructions/js/index.js
+++ instructions/js/index.js
@@ -1,17 +1,40 @@
-let today = new Date();
-let thisYear = today.getFullYear();
+const today = new Date();
+const thisYear = today.getFullYear();
 const footer= document.querySelector('footer');
 const copyright = document.createElement('p');
 copyright.textContent= `\u00A9 Mariia ${thisYear}`;
 footer.appendChild(copyright);
 
-let skills = ['HTML5', 'CSS3', 'Bootstrap', 'JavaScript'];
-let skillsSection = document.querySelector('#skills');
-let skillList = skillsSection.querySelector('ul');
+const skills = ['HTML5', 'CSS3', 'Bootstrap', 'JavaScript'];
+const skillsSection = document.querySelector('#skills');
+const skillList = skillsSection.querySelector('ul');
 
 for(let i = 0; i < skills.length; i++){
     let skill = document.createElement('li');
     skill.textContent = skills[i];
-    console.log(skill);
     skillList.appendChild(skill);
 }
+
+const messageForm = document.getElementsByName('leave_message')[0];
+   messageForm.addEventListener ('submit', (e)=>{
+    e.preventDefault();
+   const usersName = e.target.usersName;
+   const usersEmail = e.target.usersEmail;
+   const usersMessage = e.target.usersMessage;
+   const messageSection = document.getElementById('messages');
+   let messageList = messageSection.querySelector('ul');
+   let newMessage = document.createElement('li');
+   newMessage.innerHTML = `<a href="mailto:${usersEmail}">${usersName}</a>
+   <span>${usersMessage}</span>`;
+
+   const removeButton = document.createElement('button');
+   removeButton.textContent = 'remove';
+   removeButton.setAttribute('type', 'button');
+   removeButton.addEventListener('click', function() {
+   const entry = removeButton.parentNode;
+   entry.remove();
+   newMessage.appendChild(removeButton);
+   messageList.appendChild(newMessage);
+   });
+   messageForm.reset();
+});
